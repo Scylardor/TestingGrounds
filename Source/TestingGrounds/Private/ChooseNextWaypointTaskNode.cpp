2924743eff7b72ba0ea18958a7f58611b1fc3624
Scylardor <baron.alx@gmail.com>
2017-04-28 16:58:13 +0200

    Rewrite the ChooseNextWaypoint behavior tree task in C++ and use it in the Patrol behavior tree instead of the BP version

--- Source/TestingGrounds/Private/ChooseNextWaypointTaskNode.cpp
+++ Source/TestingGrounds/Private/ChooseNextWaypointTaskNode.cpp
@@ -3,18 +3,32 @@
 #include "TestingGrounds.h"
 #include "ChooseNextWaypointTaskNode.h"
 #include "BehaviorTree/BlackboardComponent.h"
-
+#include "AIController.h"
+#include "PatrolRoute.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
 	UBlackboardComponent* bbCpnt = OwnerComp.GetBlackboardComponent();
-	if (!ensure(bbCpnt != nullptr))
+	AAIController* controller = OwnerComp.GetAIOwner();
+	APawn* guardPawn = (controller ? controller->GetPawn() : nullptr);
+	UPatrolRoute* patrolRoute = (guardPawn ? guardPawn->FindComponentByClass<UPatrolRoute>() : nullptr);
+	if (!ensure(bbCpnt != nullptr && patrolRoute != nullptr))
 	{
 		return EBTNodeResult::Failed;
 	}
 
+	// Get next waypoint index
+	const TArray<AActor*>& patrolPoints = patrolRoute->GetPatrolPoints();
 	int32 nextIndex = bbCpnt->GetValueAsInt(IndexKey.SelectedKeyName);
 
+	// Set next waypoint
+	if (ensure(nextIndex < patrolPoints.Num()))
+	{
+		bbCpnt->SetValueAsObject(WaypointKey.SelectedKeyName, patrolPoints[nextIndex]);
+	}
+
+	// Cycle waypoint index
+	bbCpnt->SetValueAsInt(IndexKey.SelectedKeyName, (nextIndex + 1) % patrolPoints.Num());
 
 	return EBTNodeResult::Succeeded;
 }
